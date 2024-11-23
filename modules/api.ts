6808ff4dc3413e08f09b96f63bfa96b3cab32456
YH <imrubi5555@gmail.com>
2024-11-24 01:31:41 +0900

    feat: api 요청 interceptor 설정

--- modules/api.ts
+++ modules/api.ts
@@ -1,5 +1,23 @@
-import axios from "axios";
+import { useAuthStore } from "@/stores/authStore";
+import axios, { AxiosRequestConfig } from "axios";
 
 export const api = axios.create({
   baseURL: "http://15.164.29.113:8080/api",
+});
+
+const { accessToken, refreshToken } = useAuthStore.getState();
+
+api.interceptors.request.use(async (config) => {
+  const ignored = ["/auth/kakao/login"];
+
+  if (ignored.some((i) => i === config.url)) {
+    // 토큰 제외
+    return config;
+  } else {
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+  }
+
+  return config;
 });
