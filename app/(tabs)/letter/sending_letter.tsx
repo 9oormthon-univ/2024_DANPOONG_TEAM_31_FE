0fd1123bf96f15c06ea377cf057e76584c0a5722
eeazzy <khs2498@gmail.com>
2024-11-24 06:34:23 +0900

    feat: api 연결

--- app/(tabs)/letter/sending_letter.tsx
+++ app/(tabs)/letter/sending_letter.tsx
@@ -4,10 +4,10 @@ export default function SendingLetter() {
   return (
     <SendingAnimation
       sendingMessage="편지 전달중"
-      doneMessage1="000님의 따듯한 한마디가"
-      doneMessage2="000님에게 전달되었어요!"
-      doneExtraMessage={`00님에게 큰 힘이 되어줄꺼예요.\n다음에도 찾아와주세요.`}
-      headerbarMessage1="00님에게 편지를 보낼게요."
+      doneMessage1="따듯한 한마디가"
+      doneMessage2="전달되었어요!"
+      doneExtraMessage={`큰 힘이 되어줄꺼예요.\n다음에도 찾아와주세요.`}
+      headerbarMessage1="편지를 보낼게요."
       headerbarMessage2="편지를 전달했어요."
     />
   );
