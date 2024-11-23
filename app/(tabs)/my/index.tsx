a9238920e8aff77c3b850d5b6a97509e84af5e98
YH <imrubi5555@gmail.com>
2024-11-24 03:22:15 +0900

    feat: 마이 페이지 api 연결

--- app/(tabs)/my/index.tsx
+++ app/(tabs)/my/index.tsx
@@ -8,17 +8,43 @@ import RightArrow from "@/assets/images/icons/right_arrow.svg";
 import Stars from "@/assets/images/icons/stars.svg";
 import BlackMail from "@/assets/images/icons/black_mail.svg";
 import HeartWhaleWithShadow from "@/assets/images/icons/heart_whale_w_shadow.svg";
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/modules/api";
 
 export default function My() {
   const router = useRouter();
   const tabBarHeight = useBottomTabBarHeight();
+
+  // 유저 정보
+  const { data: myInfo } = useQuery({
+    queryFn: () => api.get("/users/myInfo").then((res) => res.data),
+    queryKey: ["users/myInfo"],
+  });
+
+  // 함께 한 지 N일
+  const { data: daysTogether } = useQuery({
+    queryFn: () => api.get("/mypage/days-together").then((res) => res.data),
+    queryKey: ["/mypage/days-together"],
+  });
+
+  // 받은 편지
+  const { data: received } = useQuery({
+    queryFn: () => api.get("/mypage/letters/received").then((res) => res.data),
+    queryKey: ["/mypage/letters/received"],
+  });
+
+  // 보낸 편지
+  const { data: sent } = useQuery({
+    queryFn: () => api.get("/mypage/letters/sent").then((res) => res.data),
+    queryKey: ["/mypage/letters/sent"],
+  });
 
   return (
     <View style={styles.container}>
       <HeaderBar title="마이페이지" />
       <View style={styles.card}>
         <View style={styles.header}>
-          <Text style={styles.username}>00님</Text>
+          <Text style={styles.username}>{myInfo?.nickname}님</Text>
           <Pressable onPress={() => router.push("/my/account")}>
             <RightArrow />
           </Pressable>
@@ -26,7 +52,7 @@ export default function My() {
         <View style={styles.message}>
           <View style={styles.daysSinceWrapper}>
             <Text style={styles.daysSince}>
-              함께한 지 <Text style={styles.days}>170</Text>일
+              함께한 지 <Text style={styles.days}>{daysTogether}</Text>일
             </Text>
             <Stars />
           </View>
@@ -36,7 +62,7 @@ export default function My() {
           <View style={styles.infoWrapper}>
             <View style={styles.chip}>
               <BlackMail />
-              <Text style={styles.amountText}>12개</Text>
+              <Text style={styles.amountText}>{received}개</Text>
             </View>
             <Text style={styles.label}>받은 편지</Text>
           </View>
@@ -44,9 +70,9 @@ export default function My() {
           <View style={styles.infoWrapper}>
             <View style={styles.chip}>
               <BlackMail />
-              <Text style={styles.amountText}>20개</Text>
+              <Text style={styles.amountText}>{sent}개</Text>
             </View>
-            <Text style={styles.label}>받은 편지</Text>
+            <Text style={styles.label}>보낸 편지</Text>
           </View>
         </View>
       </View>
