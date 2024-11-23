35e49458c55f3e82974b01e1e444ce1ca5785564
YH <imrubi5555@gmail.com>
2024-11-24 01:47:58 +0900

    fix: interceptor 수정

--- modules/api.ts
+++ modules/api.ts
@@ -5,19 +5,24 @@ export const api = axios.create({
   baseURL: "http://15.164.29.113:8080/api",
 });
 
-const { accessToken, refreshToken } = useAuthStore.getState();
+api.interceptors.request.use(
+  async (config) => {
+    const ignored = ["/auth/kakao/login"];
 
-api.interceptors.request.use(async (config) => {
-  const ignored = ["/auth/kakao/login"];
+    if (ignored.some((i) => i === config.url)) {
+      // 토큰 제외
+      return config;
+    } else {
+      const { accessToken, refreshToken } = useAuthStore.getState();
 
-  if (ignored.some((i) => i === config.url)) {
-    // 토큰 제외
-    return config;
-  } else {
-    if (accessToken) {
-      config.headers["Authorization"] = `Bearer ${accessToken}`;
+      if (accessToken) {
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
+      }
     }
-  }
 
-  return config;
-});
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
